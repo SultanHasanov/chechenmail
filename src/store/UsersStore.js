e35d7f592fcb52a0d4a69223d173a281b5459391
SultanHasanov <105391964+SultanHasanov@users.noreply.github.com>
2024-11-29 15:32:45 +0300

    Update UsersStore.js

--- src/store/UsersStore.js
+++ src/store/UsersStore.js
@@ -15,7 +15,7 @@ class UsersStore {
     this.error = null;
 
     try {
-      const response = await axios.get("http://90.156.156.78:8080/users_list");
+      const response = await axios.get("https://90.156.156.78:8080/users_list");
       this.users = response.data; // Сохраняем данные о пользователях
     } catch (err) {
       this.error = err.response?.data?.message || "Ошибка при загрузке пользователей";
